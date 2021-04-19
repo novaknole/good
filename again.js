0610d97968061aef568b7d7c2c79f77cea7ad2de
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-04-19 22:51:05 +0400

    again

--- again.js
+++ again.js
@@ -16,7 +16,7 @@ let print_all_sum_rec = function(target, current_sum, start, result, output) {
     }
 };
 
-
+//fff
 let print_all_sum = function(target) {
     let output = [];
     let result = [];
